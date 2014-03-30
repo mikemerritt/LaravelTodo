2da7afb03816200fee0ebf7f95f4b7f75267cd82
Mike Merritt <emmjemm@gmail.com>
2014-03-30 15:24:48 -0500

    delete lists

--- app/controllers/TodoController.php
+++ app/controllers/TodoController.php
@@ -2,7 +2,7 @@
 
 class TodoController extends BaseController {
 
-	// Before Filters
+  // Before Filters
   public function __construct() {
     $this->beforeFilter('csrf', array('on' => 'post'));
     $this->beforeFilter(function() {
@@ -12,81 +12,84 @@ class TodoController extends BaseController {
     });
   }
 
-	/**
-	 * Display todos for current user.
-	 *
-	 * @return Response
-	 */
-	public function index() {
-		$todos = Todo::where('user_id', '=', Auth::user()->id)->get();
-		return View::make('todo.index')->with('todos', $todos);
-	}
+  /**
+   * Display todos for current user.
+   *
+   * @return Response
+   */
+  public function index() {
+    $todos = Todo::where('user_id', '=', Auth::user()->id)->get();
+    return View::make('todo.index')->with('todos', $todos);
+  }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create() {
-		$todo = new Todo;
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function create() {
+    $todo = new Todo;
     return View::make('todo.create')->with('todo', $todo);
-	}
+  }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store() {
-		$todo = new Todo();
-		$todo->title = Input::get('title');
-		$user = User::find(Auth::user()->id);
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @return Response
+   */
+  public function store() {
+    $todo = new Todo();
+    $todo->title = Input::get('title');
+    $user = User::find(Auth::user()->id);
 
     if ($todo = $user->todos()->save($todo) ) {
       return Redirect::to('todo');
     } else {
       return View::make('todo.create')->with('todo', $todo)->with('messages', $todo->errors()->all());
     }
-	}
+  }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id) {
-		//
-	}
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function show($id) {
+    //
+  }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id) {
-		//
-	}
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function edit($id) {
+    //
+  }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id) {
-		//
-	}
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function update($id) {
+    //
+  }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id) {
-		//
-	}
+  /**
+   * Delete a list
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id) {
+    $todo = Todo::find($id);
+    if ($todo->user_id == Auth::user()->id) {
+      $todo->delete();
+    }
+  }
 
 }
--- app/views/todo/index.blade.php
+++ app/views/todo/index.blade.php
@@ -2,9 +2,11 @@
 @section('content')
   <h1>{{ Auth::user()->name }}'s Todo Lists</h1>
   {{ link_to_route('todo.create', 'Create New List', null, array('class' => 'button')) }}
-  <ul>
+  <ul class="manage-lists">
     @foreach ($todos as $todo)
-      <li>{{ $todo->title }}</li>
+      <li>{{ link_to_route('todo.show', $todo->title, array('todo' => $todo->id)) }} 
+      {{ link_to_route('todo.destroy', 'Delete List', array('todo' => $todo->id), array('class' => 'right remove', 
+      'data-method' => 'delete', 'data-confirm' => 'Are you sure?')) }}</li>
     @endforeach
   </ul>
 @stop
